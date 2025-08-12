dee515cce7c66c25857a585ececb14629bc010be
RanakOFour <4c3Quack@protonmail.com>
2025-08-12 05:47:01 +0100

    Fixed namespace

--- Runtime/DungeonGenerator.cs
+++ Runtime/DungeonGenerator.cs
@@ -2,7 +2,7 @@ using System;
 using System.Collections.Generic;
 using UnityEngine;
 
-namespace RanaksDungeonGenerator.Scripts
+namespace RanaksDunGen
 {
     [Serializable]
     struct DungeonPartContainer
